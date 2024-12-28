afa689241947874b756148ce08dd33ebf4b93477
Arsalan <khalidarsalan106@gmail.com>
2024-12-28 17:10:03 +0400

    linuxKernel.kernels.linux_zen: 6.12.6-zen1 -> 6.12.7-zen1

--- pkgs/os-specific/linux/kernel/zen-kernels.nix
+++ pkgs/os-specific/linux/kernel/zen-kernels.nix
@@ -12,9 +12,9 @@ let
   variants = {
     # ./update-zen.py zen
     zen = {
-      version = "6.12.6"; #zen
+      version = "6.12.7"; #zen
       suffix = "zen1"; #zen
-      sha256 = "0l81c0d7y3rs2a389gmsapd1j5llqvjhjsw67704ac7y83kprzn3"; #zen
+      sha256 = "1qy7r3mmgnwch926vm5w5z1dvrcgvkv4iifv1k0hqnf02vh45ry9"; #zen
       isLqx = false;
     };
     # ./update-zen.py lqx

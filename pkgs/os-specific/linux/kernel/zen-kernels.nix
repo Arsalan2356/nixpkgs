a5ceb1a3b6f8a36f62812b073fc861911a104b41
Arsalan <khalidarsalan106@gmail.com>
2025-02-09 15:18:59 -0500

    linuxKernel.kernels.linux_zen: 6.13.1-zen1 -> 6.13.2-zen1

--- pkgs/os-specific/linux/kernel/zen-kernels.nix
+++ pkgs/os-specific/linux/kernel/zen-kernels.nix
@@ -12,9 +12,9 @@ let
   variants = {
     # ./update-zen.py zen
     zen = {
-      version = "6.13.1"; #zen
-      suffix = "zen1"; #zen
-      sha256 = "1n22hsjl77qby2s4wf9y6z3kbcw7yziiniv2x43xixgkl12ajvk5"; #zen
+      version = "6.13.2"; # zen
+      suffix = "zen1"; # zen
+      sha256 = "0l3lf0arcwnv5xw62js7imir62ski9yyz1n8hjhhykq0jkpnw194"; # zen
       isLqx = false;
     };
     # ./update-zen.py lqx
